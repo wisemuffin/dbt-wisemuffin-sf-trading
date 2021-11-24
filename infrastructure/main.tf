4a3e84f50eee50c3ab51b0ada6169fcb4df6b2f3
David Griffiths <davidgg777@hotmail.com>
2021-11-25 08:34:19 +1100

    fixed up terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -69,12 +69,12 @@ provider "snowflake" {
 }
 
 
-resource "snowflake_role" "dbt_dev_role" {
+resource "snowflake_role" "dev_role" {
   provider = snowflake.security_admin
   name     = "DBT_DEV_ROLE"
 }
 
-resource "snowflake_role" "dbt_prod_role" {
+resource "snowflake_role" "prod_role" {
   provider = snowflake.security_admin
   name     = "DBT_PROD_ROLE"
 }
@@ -84,7 +84,7 @@ resource "snowflake_database_grant" "grant_dbt_hol_dev" {
   provider          = snowflake.security_admin
   database_name     = snowflake_database.dbt_hol_dev.name
   privilege         = "USAGE"
-  roles             = [snowflake_role.dbt_dev_role.name]
+  roles             = [snowflake_role.dev_role.name]
   with_grant_option = false
 }
 
@@ -92,7 +92,7 @@ resource "snowflake_database_grant" "grant_dbt_hol_prod" {
   provider          = snowflake.security_admin
   database_name     = snowflake_database.dbt_hol_prod.name
   privilege         = "USAGE"
-  roles             = [snowflake_role.dbt_prod_role.name]
+  roles             = [snowflake_role.prod_role.name]
   with_grant_option = false
 }
 
@@ -101,7 +101,7 @@ resource "snowflake_warehouse_grant" "grant_dbt_dev_wh" {
   provider          = snowflake.security_admin
   warehouse_name    = snowflake_warehouse.dbt_dev_wh.name
   privilege         = "USAGE"
-  roles             = [snowflake_role.dbt_dev_role.name]
+  roles             = [snowflake_role.dev_role.name]
   with_grant_option = false
 }
 
@@ -109,7 +109,7 @@ resource "snowflake_warehouse_grant" "grant_dbt_dev_heavy_wh" {
   provider          = snowflake.security_admin
   warehouse_name    = snowflake_warehouse.dbt_dev_heavy_wh.name
   privilege         = "USAGE"
-  roles             = [snowflake_role.dbt_dev_role.name]
+  roles             = [snowflake_role.dev_role.name]
   with_grant_option = false
 }
 
@@ -117,7 +117,7 @@ resource "snowflake_warehouse_grant" "grant_dbt_prod_wh" {
   provider          = snowflake.security_admin
   warehouse_name    = snowflake_warehouse.dbt_prod_wh.name
   privilege         = "USAGE"
-  roles             = [snowflake_role.dbt_prod_role.name]
+  roles             = [snowflake_role.prod_role.name]
   with_grant_option = false
 }
 
@@ -125,7 +125,7 @@ resource "snowflake_warehouse_grant" "grant_dbt_prod_heavy_wh" {
   provider          = snowflake.security_admin
   warehouse_name    = snowflake_warehouse.dbt_prod_heavy_wh.name
   privilege         = "USAGE"
-  roles             = [snowflake_role.dbt_prod_role.name]
+  roles             = [snowflake_role.prod_role.name]
   with_grant_option = false
 }
 
@@ -140,19 +140,19 @@ resource "snowflake_user" "user" {
   provider          = snowflake.security_admin
   name              = "dbt_user"
   default_warehouse = snowflake_warehouse.dbt_dev_wh.name
-  default_role      = snowflake_role.dbt_dev_role.name
+  default_role      = snowflake_role.dev_role.name
   rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 }
 
 
 resource "snowflake_role_grants" "grants_dev" {
   provider  = snowflake.security_admin
-  role_name = snowflake_role.dbt_dev_role.name
+  role_name = snowflake_role.dev_role.name
   users     = [snowflake_user.user.name]
 }
 
 resource "snowflake_role_grants" "grants_prod" {
   provider  = snowflake.security_admin
-  role_name = snowflake_role.dbt_prod_role.name
+  role_name = snowflake_role.prod_role.name
   users     = [snowflake_user.user.name]
 }
