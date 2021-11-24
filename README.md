c0fa4fbe7baff7b7a0b5b34e3d455bc6b4caab04
David Griffiths <davidgg777@hotmail.com>
2021-11-24 17:19:24 +1100

    Add Service User, Schema, Grants

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sfguide-terraform-sample
+
+[guide](https://quickstarts.snowflake.com/guide/terraforming_snowflake/#1)
+
+## state files
+If multiple users run Terraform, and/or if it runs on different computers, and/or if runs through CI/CD, the state files' state needs to be put in [Remote Storage](https://www.terraform.io/docs/language/state/remote.html). While using local state, you'll see the current state stored in *.tfstate and old versions named *.tfstate.*.
+
+## running
+
+```bash
+terraform plan
+```
--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ terraform {
 provider "snowflake" {
   alias = "sys_admin"
   role  = "SYSADMIN"
+  region = "ap-southeast-2"
+
 }
 
 resource "snowflake_database" "db" {
@@ -23,4 +25,75 @@ resource "snowflake_warehouse" "warehouse" {
   warehouse_size = "large"
 
   auto_suspend = 60
+}
+
+
+provider "snowflake" {
+    alias = "security_admin"
+    role  = "SECURITYADMIN"
+}
+
+
+resource "snowflake_role" "role" {
+    provider = snowflake.security_admin
+    name     = "TF_DEMO_SVC_ROLE"
+}
+
+
+resource "snowflake_database_grant" "grant" {
+    provider          = snowflake.security_admin
+    database_name     = snowflake_database.db.name
+    privilege         = "USAGE"
+    roles             = [snowflake_role.role.name]
+    with_grant_option = false
+}
+
+
+resource "snowflake_schema" "schema" {
+    provider   = snowflake.sys_admin
+    database   = snowflake_database.db.name
+    name       = "TF_DEMO"
+    is_managed = false
+}
+
+
+resource "snowflake_schema_grant" "grant" {
+    provider          = snowflake.security_admin
+    database_name     = snowflake_database.db.name
+    schema_name       = snowflake_schema.schema.name
+    privilege         = "USAGE"
+    roles             = [snowflake_role.role.name]
+    with_grant_option = false
+}
+
+
+resource "snowflake_warehouse_grant" "grant" {
+    provider          = snowflake.security_admin
+    warehouse_name    = snowflake_warehouse.warehouse.name
+    privilege         = "USAGE"
+    roles             = [snowflake_role.role.name]
+    with_grant_option = false
+}
+
+
+resource "tls_private_key" "svc_key" {
+    algorithm = "RSA"
+    rsa_bits  = 2048
+}
+
+
+resource "snowflake_user" "user" {
+    provider          = snowflake.security_admin
+    name              = "tf_demo_user"
+    default_warehouse = snowflake_warehouse.warehouse.name
+    default_role      = snowflake_role.role.name
+    default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
+    rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
+}
+
+
+resource "snowflake_role_grants" "grants" {
+    provider  = snowflake.security_admin
+    role_name = snowflake_role.role.name
+    users     = [snowflake_user.user.name]
 }
